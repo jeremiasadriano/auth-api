f9b5444f99bb23e667ba3ef234f3dc76a6084591
Jeremias Adriano <109630558+jeremiasadriano@users.noreply.github.com>
2024-05-09 16:32:54 +0200

    Playing with docker-compose

--- src/main/java/com/my/knowlodge/knowlodge01/controllers/AuthController.java
+++ src/main/java/com/my/knowlodge/knowlodge01/controllers/AuthController.java
@@ -6,7 +6,10 @@ import com.my.knowlodge.knowlodge01.security.JwtFilter;
 import com.my.knowlodge.knowlodge01.services.AuthService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,12 +22,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
     private final JwtFilter jwtFilter;
+    @Value("${app.security.token_prefix}")
+    private String TOKEN_PREFIX;
+    @Value("${app.security.cookie_name}")
+    private String COOKIE_NAME;
+
 
     @PostMapping("/login")
-    public ResponseEntity<HttpStatus> login(@RequestBody AuthRequest request, HttpServletResponse response) {
+    public ResponseEntity<HttpStatus> login(@RequestBody AuthRequest request) {
         String token = this.authService.login(request).token();
-        this.jwtFilter.createCookie(response, token);
-        return new ResponseEntity<>(HttpStatus.OK);
+        ResponseCookie cookie = ResponseCookie.from(COOKIE_NAME, TOKEN_PREFIX.concat(token))
+                .httpOnly(true)
+                .path("/")
+                .maxAge(360)
+                .secure(false)
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 
     @PostMapping("/register")
