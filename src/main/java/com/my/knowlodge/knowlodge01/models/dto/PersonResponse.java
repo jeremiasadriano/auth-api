94802b2cb6b8bfdeab7b50dca0f693b3768fc448
Jeremias Adriano <109630558+jeremiasadriano@users.noreply.github.com>
2024-07-04 15:18:04 +0200

    Improving the localData(age) logic

--- src/main/java/com/my/knowlodge/knowlodge01/models/dto/PersonResponse.java
+++ src/main/java/com/my/knowlodge/knowlodge01/models/dto/PersonResponse.java
@@ -11,11 +11,6 @@ public record PersonResponse(long id, String name, String email, int age) {
     }
 
     public static int age(LocalDate person) {
-        int year = Integer.parseInt(person.toString().substring(0, 4));
-        int month = Integer.parseInt(person.toString().substring(5, 7));
-        int day = Integer.parseInt(person.toString().substring(8, 10));
-        LocalDate birthYear = LocalDate.of(year, month, day);
-        LocalDate today = LocalDate.now();
-        return Period.between(birthYear, today).getYears();
+        return Period.between(person, LocalDate.now()).getYears();
     }
 }
